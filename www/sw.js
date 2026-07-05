7ac123fafc58e770e1247eabb9358695e4ebf13e
M2 Code System <manuelarjonacarrera@gmail.com>
2026-07-05 13:30:31 +0200

    Enhance service worker with activation event
    Added activation event and skipWaiting to service worker.

--- www/sw.js
+++ www/sw.js
@@ -1,11 +1,13 @@
-// Archivo: sw.js
-// Service Worker básico para cumplir los requisitos de instalación PWA
-
 self.addEventListener('install', (evento) => {
+    self.skipWaiting();
     console.log('[GesCar OS] Service Worker instalado correctamente.');
 });
 
+self.addEventListener('activate', (evento) => {
+    console.log('[GesCar OS] Service Worker activado.');
+});
+
+// Chrome requiere que este evento exista para mostrar el botón de instalación
 self.addEventListener('fetch', (evento) => {
-    // Aquí en el futuro podríamos programar el modo sin conexión, 
-    // pero por ahora lo dejamos pasar todo para que funcione normal.
+    // No hacemos nada, solo pasamos la red normal
 });
